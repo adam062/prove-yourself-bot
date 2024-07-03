fe9e4d9d73c143af03c68d1fab4d5d77f69a22e1
adam062 <80047476+adam062@users.noreply.github.com>
2024-07-03 10:47:49 +0300

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    bot: {
+      owners: [ "572779407773663283", "", "" ], // owners id
+      Prefix: '-',
+      Lang: 'en', // Lang ( en & ar )
+      botID: "1179495747188707428", // bot id
+      GuildId: "1175887029117124679", // Your Server Id
+      ClientId: "1179495747188707428", // Clietn Id
+      serverinvte: "https://discord.gg/hU3mJCHH4G", // Server invite url
+      clientSECRET: "Nz_vqP_68cu8-zUsoRVuVDF1eqbz1qDp" || process.env.client , // Client Secret
+      callbackURL: "https://prove-yourself--mohamedshahrour.repl.co/login", // Call Back Url
+      TOKEN: "MTE3OTQ5NTc0NzE4ODcwNzQyOA.GmmGzD.gTNqibeagxKU3wJWnG8-XNsRPrtYwWcE_SRKvI" || process.env.token // Bot Token
+    },
+    website: {
+      URL: 'https://prove-yourself--mohamedshahrour.repl.co', // URL WebSite
+      PORT: "3000", // WebSite Port
+    }
+  }
